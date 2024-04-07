0f125ea367cca3b38f60099708747bb491c8865f
Sanket Mahadik <sanketmahadik1999@gmail.com>
2024-04-07 22:31:33 +0530

    heroBanner and Ui updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,84 @@
-import { useEffect } from 'react'
-// import './App.css'
-// import {BrowserRouter} from 'react-router-dom'
-// import fetchDataFromApi from './utils/api'
-// import { useSelector,useDispatch } from 'react-redux';
-// import { getApiConfiguration } from './store/HomeSlice';
-// import Home from './pages/Home/Home';
-// import {Routes ,Route} from 'react-router-dom'
+// import { useState, useEffect } from "react";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { fetchDataFromApi } from "./utils/api";
 
-// import Header from './components/Header/Header';
-// import Footer from './components/Footer/Footer';
-// import Details from './pages/Details/Details';
-// import SearchResult from './pages/SearchResult/SearchResult'
-// import Explore from './pages/explore/Explore'
-// import PageNotFound from './pages/404/PageNotFound';
+// import { useSelector, useDispatch } from "react-redux";
+// import { getApiConfiguration, getGenres } from "./store/homeSlice";
+
+// import Header from "./components/header/Header";
+// import Footer from "./components/footer/Footer";
+// import Home from "./pages/home/Home";
+// import Details from "./pages/details/Details";
+// import SearchResult from "./pages/searchResult/SearchResult";
+// import Explore from "./pages/explore/Explore";
+// import PageNotFound from "./pages/404/PageNotFound";
+
+// function App() {
+//     const dispatch = useDispatch();
+//     const { url } = useSelector((state) => state.home);
+//     console.log(url);
+
+//     useEffect(() => {
+//         fetchApiConfig();
+//         genresCall();
+//     }, []);
+
+//     const fetchApiConfig = () => {
+//         fetchDataFromApi("/configuration").then((res) => {
+//             console.log(res);
+
+//             const url = {
+//                 backdrop: res.images.secure_base_url + "original",
+//                 poster: res.images.secure_base_url + "original",
+//                 profile: res.images.secure_base_url + "original",
+//             };
+
+//             dispatch(getApiConfiguration(url));
+//         });
+//     };
+
+//     const genresCall = async () => {
+//         let promises = [];
+//         let endPoints = ["tv", "movie"];
+//         let allGenres = {};
+
+//         endPoints.forEach((url) => {
+//             promises.push(fetchDataFromApi(`/genre/${url}/list`));
+//         });
+
+//         const data = await Promise.all(promises);
+//         console.log(data);
+//         data.map(({ genres }) => {
+//             return genres.map((item) => (allGenres[item.id] = item));
+//         });
+
+//         dispatch(getGenres(allGenres));
+//     };
+
+//     return (
+//         <BrowserRouter>
+//             <Header />
+//             <Routes>
+//                 <Route path="/" element={<Home />} />
+//                 <Route path="/:mediaType/:id" element={<Details />} />
+//                 <Route path="/search/:query" element={<SearchResult />} />
+//                 <Route path="/explore/:mediaType" element={<Explore />} />
+//                 <Route path="*" element={<PageNotFound />} />
+//             </Routes>
+//             <Footer />
+//         </BrowserRouter>
+//     );
+// }
+
+// export default App;
+
 
 
 function App() {
-
-  // const dispatch = useDispatch();
-  // const {url} =useSelector((state)=> state.home);
- 
-
-  // useEffect(()=>{
-  //   apitesting();
-  // })
-
-  // const apitesting = () => {
-  //   fetchDataFromApi('/movie/popular')
-  //   .then((res)=>{
-  //     console.log(res);
-  //     dispatch(getApiConfiguration(res))
-  //   })
-  // }
-
   return (
-     <h1>Movies App</h1>
-  //  <BrowserRouter>
-  //   <Home></Home>
-  //  <Header></Header>
-  //    <Routes>
-  //         <Route path="/" element={<Home />} />
-  //         <Route path="/:mediaTYpe/:id" element={<Details />} />
-  //         <Route path="/search/:query" element={<SearchResult />} />
-  //         <Route path="/explore/:mediaType" element={<Explore />} />
-  //         <Route path="*" element={<PageNotFound />} />
-  //    </Routes>
-  //    <Footer></Footer>
-  //  </BrowserRouter>
+    <div>
+      <h1>Temp</h1>
+    </div>
   )
 }
 
--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,11 +1,18 @@
+import React from "react";
 
+import "./style.scss";
 
-function PageNotFound() {
-  return (
-    <div>
-      <h3>Page eroor 404</h3>
-    </div>
-  )
-}
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
+const PageNotFound = () => {
+    return (
+        <div className="pageNotFound">
+            <ContentWrapper>
+                <span className="bigText">404</span>
+                <span className="smallText">Page not found!</span>
+            </ContentWrapper>
+        </div>
+    );
+};
 
 export default PageNotFound;
