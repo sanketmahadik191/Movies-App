75e9f0f08273f683988a4336682e26638c7c7c46
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2024-08-27 14:24:53 +0530

    v3

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 
-import homeSlice from "./homeSlice.js";
+import homeSlice from "../store/homeSlice.js";
 
 export const store = configureStore({
     reducer: {
